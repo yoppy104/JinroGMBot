508e6d7cc63a23317f97adcd47971589827d7726
Yotsuhi <toho_momiji_104@yahoo.co.jp>
2021-02-04 21:51:13 +0900

    [add] チャンネルを追加する機能をConnecterに追加

--- Main.py
+++ Main.py
@@ -6,10 +6,21 @@ connecter = Connecter()
 command = Command()
 
 
+# Command用メソッド
 async def PingPong(message):
     await connecter.Reply(message, "pong")
 
+async def MKChannel(message):
+    name = message.content.split(" ")[1]
+    new_ch = await connecter.createChannelFromMessage(message, name)
+    connecter.addChannelIDs(name, new_ch.id)
+    await connecter.Reply(message, "チャンネル<<{}>>を作成しました。".format(name))
+    await connecter.Reply(new_ch.mention, new_ch, "作成しました。")
+
+# Commandの登録
 command.addCommand("!ping", PingPong)
+command.addCommand("!mkch", MKChannel)
+
 
 # 接続時に起動
 @connecter.client.event
--- System/Connecter.py
+++ System/Connecter.py
@@ -22,17 +22,38 @@ class Connecter:
         f.close()
         return out
 
-    
+    def addChannelIDs(self, name, ch_id):
+        self.channel_ids[name] = ch_id
+
+    # channelIDを取得する。
     def GetChannel(self, channel_name):
         ch_id = self.channel_ids[channel_name]
         return self.client.get_channel(ch_id)
 
 
-    async def Send(self, channel,content):
+    # 特定のチャンネルにメッセージを送信する。
+    async def Send(self, channel, content):
         await channel.send(content)
 
 
+    # メンション付きで返信する。
     async def Reply(self, message, content):
-        reply = "{} {}".format(message.author.mention, content)
-        await self.Send(message.channel, reply)
+        await self.Reply(message.author.mention, message.channel, reply)
+
+    # メンション付きで送信する。
+    async def Reply(self, mention, channel, content):
+        reply = "{} {}".format(mention, content)
+        await self.Send(channel, reply)
+
+    
+    # messageからチャンネルの生成を行う
+    async def createChannelFromMessage(self, message, channel_name):
+        category_id = message.channel.category_id
+        category = message.guild.get_channel(category_id)
+        return await self.createChannel(category, channel_name)
+
+    # カテゴリにチャンネルを作成する。
+    async def createChannel(self, category, channel_name):
+        new_channel = await category.create_text_channel(name=channel_name)
+        return new_channel
 
