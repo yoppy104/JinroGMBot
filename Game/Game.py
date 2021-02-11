dbcbbc9cc07d8e2b09d3b882854d52799be86269
Yotsuhi <toho_momiji_104@yahoo.co.jp>
2021-02-11 23:01:11 +0900

    [add] ゲーム開始時にチャンネルを一通り作成して、開始通知を送信する

--- Game/Game.py
+++ Game/Game.py
@@ -3,6 +3,7 @@ BASIC_CREATE_TEXT_CHANNELS_NAME = [
     "霊界",
     "掲示板"
 ]
+
 BASIC_CREATE_VOICE_CHANNELS_NAME = [
     "討論場"
 ]
@@ -33,7 +34,7 @@ class Game:
     async def CreateTextChannels(self, channel_list):
         for text_ch in channel_list:
             new_ch = await self.connecter.CreateTextChannel(self.use_category, text_ch)
-            self.channels[new_ch] = new_ch
+            self.channels[text_ch] = new_ch
     
     # チャンネル名のリストからボイスチャンネルを作成して、スタックする。
     async def CreateVoiceChannels(self, channel_list):
@@ -47,7 +48,7 @@ class Game:
         self.use_category = await self.connecter.CreateCategory(self.connecter.guild, "人狼ゲーム")
 
         # 基本的に使用するチャンネルを一括で作成
-        await self.CreateTextChannels(BASIC_CREATE_TEXT_CHANNEL_NAMES)
+        await self.CreateTextChannels(BASIC_CREATE_TEXT_CHANNELS_NAME)
         await self.CreateVoiceChannels(BASIC_CREATE_VOICE_CHANNELS_NAME)
 
         await self.connecter.Reply(message.author.mention, message.channel, "{}で参加者を募ります".format("掲示板"))
@@ -55,12 +56,12 @@ class Game:
         game_main_channel = self.channels["掲示板"]
         join_emoji = EMOJI["join"]
         finish_emoji = EMOJI["finish"]
+        self.command.addSendEmoji(game_main_channel, [join_emoji, finish_emoji])
         await self.connecter.Send(
             game_main_channel,
             "人狼ゲームを行います。参加する場合は{}を押してください。\n\n人数が揃ったら、{}を押してください。"\
                 .format(join_emoji, finish_emoji)
             )
-        await self.command.addSendEmoji(game_main_channel, [join_emoji, finish_emoji])
 
 
     
